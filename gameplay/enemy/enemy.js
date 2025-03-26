b672be88ff7c93d746f71a809b6a4284beadb33c
klu0926 <lukuoyu@gmail.com>
2025-03-26 16:01:08 -0400

    use css to animate enemy movement

--- gameplay/enemy/enemy.js
+++ gameplay/enemy/enemy.js
@@ -35,23 +35,38 @@ function getEnemyFromList(level) {
   return enemyList[level]
 }
 
-
-// gameScreen is the #game_screen div
+// gameScreen is the #game_screen div 
 export class Enemy {
-  constructor(gameScreen, level,) {
+  constructor(gameScreen, level = 1) {
+    // location/styles
+    this.gameScreen = gameScreen;
     this.outerClass = 'enemy'
     this.innerClass = 'enemy-sprit'
-    this.innerColor = 'red' // for testing
-    this.hp = enemyList(level);
-    this.speed = speed;
-    this.damage = damage;
-    this.gameScreen = gameScreen;
+    this.outerDiv = null
+    this.innerDiv = null
     this.width = 40;
     this.height = 40;
-    this.x = gameScreen.offsetWidth;
-    this.y = 100; // default vertical position
+    this.x = gameScreen.offsetWidth; // deault x position
+    this.y = 70; // default y position
+    this.isMoving = false;
 
-    // Outer div (for position)
+    // states
+    this.name = getEnemyFromList(level).name
+    this.hp = getEnemyFromList(level).hp
+    this.speed = getEnemyFromList(level).speed;
+    this.damage = getEnemyFromList(level).damage;
+    this.innerColor = getEnemyFromList(level).color
+
+
+    // bind methods just in case
+    this.spawn = this.spawn.bind(this)
+    this.move = this.move.bind(this)
+    this.takeDamage = this.takeDamage.bind(this)
+    this.destroy = this.destroy.bind(this)
+    this.attack = this.attack.bind(this)
+  }
+  spawn() {
+    // Creat outter div
     this.outer = document.createElement('div');
     this.outer.classList.add(this.outerClass)
     this.outer.style.position = 'absolute';
@@ -59,29 +74,23 @@ export class Enemy {
     this.outer.style.height = `${this.height}px`;
     this.outer.style.left = `${this.x}px`;
     this.outer.style.top = `${this.y}px`;
+    this.outerDiv = this.outer
 
-    // Inner div (for sprite/image)
+    // Create inner div
     this.inner = document.createElement('div');
     this.inner.classList.add(this.innerClass);
     this.inner.style.width = '100%';
     this.inner.style.height = '100%';
     // Temp color so we can see it
     this.inner.style.backgroundColor = this.innerColor;
+    this.innerDiv = this.inner
 
-    // put it togeher
+    // put it on screen
     this.outer.appendChild(this.inner);
     this.gameScreen.appendChild(this.outer);
-
-    // bind methods just in case
-    this.move = this.move.bind(this)
-    this.takeDamage = this.takeDamage.bind(this)
-    this.destroy = this.destroy.bind(this)
-    this.attack = this.attack.bind(this)
   }
   move() {
-    // move toward left
-    this.x -= this.speed;
-    this.outer.style.left = `${this.x}px`;
+    this.outerDiv.classList.add('move')
   }
   takeDamage(amount) {
     console.log(`Enemy took damage: ${amount}`)
--- gameplay/game/game.js
+++ gameplay/game/game.js
@@ -6,6 +6,7 @@ function getLevelState(level = 1) {
   // base 
   const baseEnemyCount = 5
   const baseEnemySpeed = 1
+  const baseEnmeySpawnTime = 1000
 
   // how many level to incrase
   const levelToIncraseCount = 2
@@ -17,9 +18,10 @@ function getLevelState(level = 1) {
 
   const enemyCount = baseEnemyCount + Math.floor((level - 1) / levelToIncraseCount) * countIncrase
   const ememySpeed = baseEnemySpeed + Math.floor((level - 1) / levelToIncraseSpeed) * speedIncrase
+  const ememySpawnTime = baseEnmeySpawnTime // just 3 second for now
 
   return {
-    enemyCount, ememySpeed
+    enemyCount, ememySpeed, ememySpawnTime
   }
 }
 
@@ -27,19 +29,69 @@ export class Game {
   constructor(gameScreen, level = 1, speedMotifier = 1) {
     // #game_screen div
     this.gameScreen = gameScreen
+
+    // game status
+    this.lastTimestamp = 0;
+    this.spawnTimer = 0;
+
     // player related
     this.points = 0
     this.baseHp = 10
     this.fireWallHp = 3
     // enemy related
+    this.enemyArray = [] // all the enemy in this level (add level control later eg: [1, 1, 1, 2, 1, 1])
     this.enemyCount = getLevelState(level).enemyCount
-    this.ememySpeed = getLevelState(level).ememySpeed
+    this.ememySpeed = getLevelState(level).ememySpeed * speedMotifier
+    this.enemySpawnTime = getLevelState(level).ememySpawnTime
+
+    // bind methods just in case
+    this.start = this.start.bind(this)
+    this.update = this.update.bind(this)
+  }
+  // Setup enemies at the beginning
+  setup() {
+    for (let i = 0; i < this.enemyCount; i++) {
+      const enemy = new Enemy(this.gameScreen, 1); // level 1
+      enemy.spawn()
+      this.enemyArray.push(enemy);
+      console.log('enemy spawn')
+    }
+    console.log('Initial Enemies:', this.enemyArray);
+  }
+
+  // Main game loop
+  update(timestamp) {
+    // timestamp is from requestAnimationFrame, its the time since game start
+    if (!this.lastTimestamp) this.lastTimestamp = timestamp;
+    // delta time is time since last frame
+    // can use delta to motify movment speed (so computer has the same speed)
+    const delta = timestamp - this.lastTimestamp;
+    this.lastTimestamp = timestamp;
+
+    // move the next enemy
+    this.spawnTimer += delta;
+    if (this.spawnTimer >= this.enemySpawnTime) {
+      const nextEnemy = this.enemyArray.find(enemy => !enemy.isMoving);
+      if (nextEnemy) {
+        nextEnemy.move();
+        nextEnemy.isMoving = true;
+      }
+      this.spawnTimer = 0; // reset spawn timer
+    }
+    requestAnimationFrame(this.update);
+  }
+  start() {
+    this.setup();
+    requestAnimationFrame(this.update);
   }
 }
 
 
 // Testing
 document.addEventListener('DOMContentLoaded', () => {
-  const game = new Game(null, 2)
-  console.log(game)
+  // #game_screen
+  const gameScreen = document.querySelector('#game_screen')
+  const game = new Game(gameScreen, 2)
+  game.start()
+
 })
