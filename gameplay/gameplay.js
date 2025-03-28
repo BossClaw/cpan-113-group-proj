955ffb158ba1bf1055970b1b0155bfe9bfbdb92d
Jasmine <jayjadesanders@gmail.com>
2025-03-28 13:19:18 -0400

    Added temporary redirection code if nothing in local storage

---
+++ gameplay/gameplay.js
@@ -0,0 +1,8 @@
+
+// Check local storage
+
+if(!localStorage.getItem("pickedLanguages")){
+    // TEMPORARY CODE - go back to mission configure page to pick languages
+    window.location.href = "http://127.0.0.1:5502/missionConfigure.html"
+}
+
