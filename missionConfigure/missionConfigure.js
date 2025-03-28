8ab50d50c31749fb3892bf9d29f2a1f6c098d3c2
Jasmine <jayjadesanders@gmail.com>
2025-03-28 12:23:21 -0400

    made linked js type module

--- missionConfigure/missionConfigure.js
+++ missionConfigure/missionConfigure.js
@@ -5,14 +5,27 @@
 
 const languageButtons = document.getElementsByClassName("language");
 let pickedLanguages = []
-
+console.log(languageButtons)
+console.log(typeof languageButtons)
 Array.from(languageButtons).forEach((button) => {
+    console.log(button)
     button.addEventListener("click", function(event){
-       toggleLanguageAddToList(event.target)
+        console.log(event.target)
+       toggleActive(event.target)
+       toggleAddToList(event.target)
     })
 })
+    
 
-function toggleLanguageAddToList(button){
+function toggleActive(button){
+    if(button.classList.contains("active-language")){
+        button.classList.remove("active-language")
+    }else{
+        button.classList.add("active-language")
+    }
+}
+
+function toggleAddToList(button){
     let selected = button.innerHTML
     if(pickedLanguages.includes(selected)){
         let index = pickedLanguages.indexOf(selected)
