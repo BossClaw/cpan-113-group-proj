ccad23d070a783119984614d9365435531f88226
Jasmine <jayjadesanders@gmail.com>
2025-03-28 13:30:18 -0400

    Added word display div in html

--- gameplay/gameplay.js
+++ gameplay/gameplay.js
@@ -1,8 +1,19 @@
 
+let pickedLanguages = null
+let index = 0
+
 // Check local storage
 
 if(!localStorage.getItem("pickedLanguages")){
     // TEMPORARY CODE - go back to mission configure page to pick languages
     window.location.href = "http://127.0.0.1:5502/missionConfigure.html"
+}else {
+    pickedLanguages = localStorage.getItem("pickedLanguages")
 }
+
+// Import words.json
+const response = await fetch("./gameplay/words.json");
+const data = await response.json();
+let wordList = data.words
+console.log(wordList)
 
