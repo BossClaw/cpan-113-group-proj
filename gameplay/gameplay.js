e1bb60ce5f987c63398b3a91066f11b44798e0cd
Jasmine <jayjadesanders@gmail.com>
2025-03-28 14:12:09 -0400

    Added css to word, logic for word to regenerate

--- gameplay/gameplay.js
+++ gameplay/gameplay.js
@@ -1,6 +1,9 @@
-
+// Variables for word 
 let pickedLanguages = []
-let index = 0
+let letterToTypeIndex = 0
+let displayedWord = ""
+const wordLetters = document.getElementById("word-container")
+
 
 
 // Import words.json
@@ -9,7 +12,6 @@ async function importWords(){
        const response = await fetch("./gameplay/words.json")
         const data = await response.json()
         let wordList = data.words
-        console.log(wordList) 
         return wordList
     }catch(error){
         console.error(error)
@@ -26,34 +28,12 @@ if(!localStorage.getItem("pickedLanguages")){
     // TEMPORARY CODE - go back to mission configure page to pick languages
     window.location.href = "http://127.0.0.1:5502/missionConfigure.html"
 }else {
-    pickedLanguages = localStorage.getItem("pickedLanguages")
-    console.log("picked languages", pickedLanguages)
+    pickedLanguages = JSON.parse(localStorage.getItem("pickedLanguages"))
     generateWord()
-    
-}
-
-
-
-// Generate a new word
-function generateWord(){
-    let randomIndex = Math.floor(Math.random() * pickedLanguages.length)
-    console.log(randomIndex)
-    let randomLanguage = pickedLanguages[randomIndex]
-    console.log("random languages", randomLanguage)
-
-    // Random Word
-    let languageWords = wordList[randomLanguage]
-    console.log(wordList[randomLanguage])
-    randomIndex = Math.floor(Math.random() * languageWords.length)
-    word = languageWords[randomIndex]
-
-    displayWord(word)
 }
 
 // Display word function
-const wordLetters = document.getElementById("word-container")
 function displayWord(word){
-    console.log(wordLetters)
     for(let char of word){
         const span = document.createElement("span")
         const node = document.createTextNode(char)
@@ -61,3 +41,52 @@ function displayWord(word){
         wordLetters.appendChild(span)
     }
 }
+
+// Clear the word, reset word index
+function clearWordDisplay() {
+    wordLetters.innerHTML = ""
+    letterToTypeIndex = 0
+}
+
+// Generate a new word
+function generateWord(){
+    // Get random language from picked language
+    let randomIndex = Math.floor(Math.random() * pickedLanguages.length)
+    let randomLanguage = pickedLanguages[randomIndex]
+
+
+    // Get random word from language
+    let languageWords = wordList[randomLanguage]
+    randomIndex = Math.floor(Math.random() * languageWords.length)
+    displayedWord = languageWords[randomIndex]
+
+    displayWord(displayedWord)
+}
+
+
+// Update letter display, if correct letter typed, change color
+function updateDisplay(key){
+    let letterToType = wordLetters.getElementsByTagName("span")[letterToTypeIndex]
+    if (key === letterToType.innerHTML){
+            letterToType.style.color = "green"
+            letterToTypeIndex += 1
+        }
+
+    checkForCompletion(letterToTypeIndex)
+}
+
+
+// Check if word is fully typed, if it is then clear word and generate new word
+function checkForCompletion(letterToTypeIndex){
+    if (letterToTypeIndex === displayedWord.length){
+        clearWordDisplay()
+        generateWord()
+    } 
+}
+
+
+// EVENT LISTENER ON PAGE KEYDOWN
+document.addEventListener("keydown", event => {
+    event.preventDefault()
+    updateDisplay(event.key)
+})
