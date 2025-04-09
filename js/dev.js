332a8931b1309ebb119988c7e0531a72abf769c5
Claw <bossclawest@outlook.com>
2025-04-09 18:25:18 -0400

    dev: Make dev style css, update visibility

--- js/dev.js
+++ js/dev.js
@@ -39,16 +39,9 @@ let fps_div;
 
 export function dev_add_fps() {
   fps_div = document.createElement("div");
-  fps_div.style.position = "fixed";
-  fps_div.style.top = "138px";
-  fps_div.style.left = "10px";
-  fps_div.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
-  fps_div.style.color = "lime";
-  fps_div.style.fontFamily = "monospace";
-  fps_div.style.padding = "5px 10px";
-  fps_div.style.borderRadius = "5px";
-  fps_div.style.zIndex = "9999";
-
+  fps_div.classList.add('dev_div'); 
+  fps_div.classList.add('dev_div_fps'); 
+  
   document.body.appendChild(fps_div);
 
   function updateFPS(currentTime) {
@@ -76,23 +69,11 @@ let mesg_div;
 // ADD DOM IF NOT ALREADY
 export function dev_log_mesg(mesg_str) {
   if (!mesg_div) {
-    mesg_div = document.createElement("div");
-    mesg_div.style.position = "fixed";
-    mesg_div.style.top = "0px";
-    mesg_div.style.left = "0px";
-    mesg_div.style.height = "128px";
-    mesg_div.style.width = "100%";
-    mesg_div.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
-    mesg_div.style.color = "orange";
-    mesg_div.style.fontFamily = "monospace";
-    mesg_div.style.fontSize = "12px";
-    mesg_div.style.padding = "5px 10px";
-    mesg_div.style.borderRadius = "5px";
-    mesg_div.style.zIndex = "9999";
-    mesg_div.style.overflow = "scroll";
-    mesg_div.style.whiteSpace = "pre";
-    mesg_div.style.overscrollBehavior = "contain";
+    mesg_div = document.createElement("div");    
+    mesg_div.classList.add("dev_div");
+    mesg_div.classList.add("dev_div_mesg");
 
+    
     document.body.appendChild(mesg_div);
   }
 
