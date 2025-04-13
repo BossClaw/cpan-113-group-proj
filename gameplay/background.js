a2a254ce34f2f11997dbde1c52bb5558f65d5b74
Claw <bossclawest@outlook.com>
2025-04-13 18:26:04 -0400

    background: tidy background.js and remove temp dev toggle

--- gameplay/background.js
+++ gameplay/background.js
@@ -2,26 +2,32 @@
 // DUCK - BACKGROUND.JS FOR DYNAMIC BACKGROUND STUFF
 // CALLED FROM gameview.js AND game.js
 
-export function set_background_glitch(gameScreen) {
+export function set_background_glitch(_level, _gameScreen) {
+  // V2DO - USE LEVEL TO 'UNLOCK BGS'
+
+  // TODO - ENSURE BG CYCLING, EG - STORE IN LOCAL STORAGE
+
   let bg_idx = `` + Math.floor(Math.random() * 8).toFixed(0);
   bg_idx = bg_idx.padStart(2, "0");
-  
+
   // HACK - TEST SPECIFIC
-  bg_idx = '05';  
-  
+  // bg_idx = "05";
+
   // TODO - FIX URL FOR LOCAL AND GITHUB
   const bg_url = `url("../gameplay/background/bg_${bg_idx}_glitch.gif")`;
   console.log(
-    `[GAMEVIEW] MADE RAND BG[${bg_url}] FOR[${gameScreen}] ID[${gameScreen.id}]`
+    `[GAMEVIEW][BG] MADE RAND BG[${bg_url}] FOR[${_gameScreen}] ID[${_gameScreen.id}]`
   );
-  gameScreen.style.backgroundImage = bg_url;
+  _gameScreen.style.backgroundImage = bg_url;
 }
 
-export function set_background_win(gameScreen) {
+export function set_background_win(_gameScreen) {
+  // V2DO - DETERMINE IF NECESSARY DUE TO DARK BG OVERLAY
+
   // JUST RENAME THE STYLE
-  let bg_url_glitch = gameScreen.style.backgroundImage;
+  let bg_url_glitch = _gameScreen.style.backgroundImage;
   let bg_url_won = bg_url_glitch.replace("_glitch.gif", "_won.png");
 
-  console.log(`[GAMEVIEW] GLITCH BG[${bg_url_glitch}] MADE WIN[${bg_url_won}]`);
-  gameScreen.style.backgroundImage = bg_url_won;
+  console.log(`[GAMEVIEW][BG] GLITCH BG[${bg_url_glitch}] MADE WIN[${bg_url_won}]`);
+  _gameScreen.style.backgroundImage = bg_url_won;
 }
