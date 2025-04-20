1d24b563616fcbd975ae4b2891c11d57ab1bf26e
Claw <bossclawest@outlook.com>
2025-04-20 01:10:19 -0400

    leaderboard: change to table to simplify css, responsive perf & js build logic

--- leaderboard.js
+++ leaderboard.js
@@ -1,105 +1,114 @@
-// to get localStorage scores and update the leaderboard 
+// to get localStorage scores and update the leaderboard
 const dummyScores = [
-  {
-    // not recorded
-    name: 'BOS',
-    score: 112300, // number
-    maxLevel: '10', // string
-    language: ["Python", "JavaScript", "CSS"],
-    difficulty: 'easy',
-    date: new Date(2025, 4, 8)
-  },
-  {
-    name: 'JAS',
-    score: 57900,
-    maxLevel: '6',
-    language: ["Python", "JavaScript", "CSS"],
-    difficulty: 'normal',
-    date: new Date(2025, 4, 8)
-  },
-  {
-    name: 'LOL',
-    score: 74200,
-    maxLevel: '7',
-    language: ["JavaScript"],
-    difficulty: 'normal',
-    date: new Date(2025, 4, 7)
-  },
-  {
-    name: 'CHO',
-    score: 133900,
-    maxLevel: '12',
-    language: ["Python", "JavaScript"],
-    difficulty: 'easy',
-    date: new Date(2025, 4, 8)
-  }
-]
+	{
+		// not recorded
+		name: 'BOS',
+		score: 112300, // number
+		maxLevel: '10', // string
+		language: ['Python', 'JavaScript', 'CSS'],
+		difficulty: 'easy',
+		date: new Date(2025, 4, 8),
+	},
+	{
+		name: 'JAS',
+		score: 57900,
+		maxLevel: '6',
+		language: ['Python', 'JavaScript', 'CSS'],
+		difficulty: 'normal',
+		date: new Date(2025, 4, 8),
+	},
+	{
+		name: 'LOL',
+		score: 74200,
+		maxLevel: '7',
+		language: ['JavaScript'],
+		difficulty: 'normal',
+		date: new Date(2025, 4, 7),
+	},
+	{
+		name: 'CHO',
+		score: 133900,
+		maxLevel: '12',
+		language: ['Python', 'JavaScript'],
+		difficulty: 'easy',
+		date: new Date(2025, 4, 8),
+	},
+];
 
 function updateLocalLeaderboard(limit = 10) {
-  // get local scores
-  const localScoresString = localStorage.getItem('settings_scores')
-  const localScores = JSON.parse(localScoresString) || []
+	// get local scores
+	const localScoresString = localStorage.getItem('settings_scores');
+	const localScores = JSON.parse(localScoresString) || [];
 
-  // combine dummy score with local scores
-  const scores = [...localScores, ...dummyScores]
+	// combine dummy score with local scores
+	const scores = [...localScores, ...dummyScores];
 
-  // sort highest score on top
-  scores.sort((a, b) => b.score - a.score)
+	// sort highest score on top
+	scores.sort((a, b) => b.score - a.score);
 
-  // only show records within limit
-  scores.splice(limit + 1)
+	// only show records within limit
+	scores.splice(limit + 1);
 
-  // display on leader board
-  const leaderboardDiv = document.querySelector('#leaderboard')
-  // clean
-  leaderboardDiv.innerHTML = ''
+	// display on leader board
+	const leaderboardTableBody = document.querySelector('#leaderboardTableBody');
+	leaderboardTableBody.innerHTML = '';
 
-  for (let i = 0; i < scores.length; i++) {
-    // score text
-    const scoreRecord = scores[i]
-    const scoreText = String(scoreRecord.score).padStart(8, '0')
-    // placement text
-    let placement
-    if (i === 0) {
-      placement = `1ST`
-    } else if (i === 1) {
-      placement = `2ND`
-    } else if (i === 2) {
-      placement = `3RD`
-    } else {
-      placement = `${i + 1}TH`
-    }
+	// LOOP THROUGH SCORES
+	for (let i = 0; i < scores.length; i++) {
+		// score text
+		const scoreRecord = scores[i];
+		//const scoreText = String(scoreRecord.score).padStart(8, '0');
+		const scoreText = String(scoreRecord.score);
 
-    // create elements
-    // place
-    const placeSpan = document.createElement('span')
-    placeSpan.innerText = placement
-    // level
-    const levelSpan = document.createElement('span')
-    levelSpan.innerText = `lv.${scoreRecord.maxLevel}`
-    // difficulty
-    const diffSpan = document.createElement('span')
-    diffSpan.innerText = scoreRecord.difficulty
-    // score
-    const scoreSpan = document.createElement('span')
-    scoreSpan.innerText = scoreText
-    // name
-    const nameSpan = document.createElement('span')
-    nameSpan.innerText = scoreRecord.name
+		// placement text
+		let placement;
+		if (i === 0) {
+			placement = `1ST`;
+		} else if (i === 1) {
+			placement = `2ND`;
+		} else if (i === 2) {
+			placement = `3RD`;
+		} else {
+			placement = `${i + 1}TH`;
+		}
 
-    // create h2
-    const scoreH2 = document.createElement('h2')
-    scoreH2.appendChild(placeSpan)
-    scoreH2.appendChild(levelSpan)
-    scoreH2.appendChild(diffSpan)
-    scoreH2.appendChild(scoreSpan)
-    scoreH2.appendChild(nameSpan)
+		// create elements
+		// place
+		const placeTd = document.createElement('td');
+		placeTd.innerText = placement;
 
-    // append
-    leaderboardDiv.appendChild(scoreH2)
-  }
+		// level
+		const levelTd = document.createElement('td');
+		//levelTd.innerText = `lv.${scoreRecord.maxLevel}`;
+		levelTd.innerText = String(scoreRecord.maxLevel).padStart(3,'0');
+
+		// difficulty
+		const diffTd = document.createElement('td');
+		diffTd.innerText = scoreRecord.difficulty;
+
+		// score
+		const scoreTd = document.createElement('td');
+		scoreTd.classList.add('prSco');
+		scoreTd.innerText = scoreText;
+
+		// name
+		const nameTd = document.createElement('td');
+		nameTd.innerText = scoreRecord.name;
+
+		// create SCORE TABLE ROW
+		const scoreTr = document.createElement('tr');
+    scoreTr.classList.add(`lb${i + 1}`);
+		scoreTr.appendChild(placeTd);
+		scoreTr.appendChild(levelTd);
+		scoreTr.appendChild(diffTd);
+		scoreTr.appendChild(scoreTd);
+		scoreTr.appendChild(nameTd);
+
+		// A BODY INNERHTML TO TABLE ROWS
+		leaderboardTableBody.appendChild(scoreTr);
+	}
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  updateLocalLeaderboard()
-})
+	updateLocalLeaderboard();
+});
